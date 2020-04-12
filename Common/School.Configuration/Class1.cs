cdc05a6dce33970defe7cc03afe1952d57a2302f
Mohamed Rady <rady.net@hotmail.com>
2020-04-12 18:16:21 +0700

    Initial Commit

---
+++ Common/School.Configuration/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace School.Configuration
+{
+    public class Class1
+    {
+    }
+}
---
+++ UI/identity-app/src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'identity-app'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('identity-app');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('identity-app app is running!');
+  });
+});
